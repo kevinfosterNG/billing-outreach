d629e7fcd00f4a9560d57c42c361556e780da1c2
Kevin Foster <kevinfoster@nextcare.com>
2023-04-13 16:01:07 -0400

    Import files from Vercel example app (https://github.com/shadcn/taxonomy.git/trunk)  + Migrated any existing files into the corresponding folder

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 import Image from "next/image";
 import React, { useState } from "react";
 // import Logo from "./Logo";
-import NavItem from "./NavItem";
-import './NavItems.css';
+import NavItem from "@/components/ui/nav";
+import '@/styles/navitems.css';
 
 const MENU_LIST = [
   { text: "Home", href: "/" },
