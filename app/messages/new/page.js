2e20b9e742d96925b6f231f7c3b4b01018f0d9f5
Kevin Foster <kevinfoster@nextcare.com>
2023-04-15 16:51:07 -0400

    Swapped out the hardcoded values for /messages/new with data dynamically driven from the MessageMaster container.

--- app/messages/new/page.js
+++ app/messages/new/page.js
@@ -1,7 +1,13 @@
+import { CosmosClient, Database, Container } from "@azure/cosmos";
+
 const newMessageUrl = "/api/messages/sendMessage"
 //const newMessageUrl = "http://io-interface01:8787/api/messages/sendMessage"
 
-export default function MessagePage() {
+export  default async function MessagePage() {
+
+    const messageOptions = await getMessageOptions("MessageMaster") || [];
+    console.log(messageOptions[0]);
+
     return (
       <form action={newMessageUrl} method="post" name="formNewMessage">
 
@@ -13,18 +19,61 @@ export default function MessagePage() {
 
         <br />
 
-        <label htmlFor="message">Message:</label>
-        <select name="message">
-          <option value={process.env.BILLING_MESSAGE_OPTIONS_TEST}>Email Option 1A</option>
-          <option value={process.env.BILLING_MESSAGE_OPTIONS_1A}>Email Option 1A</option>
-          <option value={process.env.BILLING_MESSAGE_OPTIONS_1B}>Email Option 1B</option>
-          <option value={process.env.BILLING_MESSAGE_OPTIONS_2A}>Email Option 2A</option>
-          <option value={process.env.BILLING_MESSAGE_OPTIONS_2B}>Email Option 2B</option>
-          <option value={process.env.BILLING_MESSAGE_OPTIONS_3A}>Email Option 3A</option>
-          <option value={process.env.BILLING_MESSAGE_OPTIONS_3B}>Email Option 3B</option>
+        <select name="messageOptions">
+          {messageOptions.map(o => <option value={o.id}>{o.description}</option>)}
         </select>
+
+        <br />
         
         <button type="submit">Submit</button>
       </form>
     )
+}
+
+async function getMessageOptions(containerName) {
+  //console.log("getMessageOptions() called...");
+
+  //console.log("getCosmosClient called...");
+  const client = getCosmosClient();
+  //console.log("Got client: ",client);
+  
+  //console.log("getCosmosContainer called...");
+  const container = getCosmosContainer(client, containerName)
+  //console.log("Got container: ", container);
+
+  //Get data out of container
+  const data = await getContainerData(container)
+    //.then((d) => console.log("Data: ", d));
+
+  //console.log("getMessageOptions() returning...");
+  return data;
+}
+
+function getCosmosClient() {
+  return new CosmosClient({
+    key: process.env.COSMOSDB_KEY,
+    endpoint: process.env.COSMOSDB_ENDPOINT,
+  });
+}
+
+function getCosmosContainer(client, container) {
+  return client
+  .database(process.env.COSMOSDB_DATABASE)
+  .container(container);
+}
+
+async function getContainerData(container){
+  // query to return all children in a family
+  const querySpec = {
+    query: 'SELECT * FROM c ',  //where c.id=@itemId
+    // parameters: [
+    //   {
+    //     name: "@itemId",
+    //     value: itemId
+    //   }
+    // ]
+  }
+  const { resources } = await container.items.query(querySpec).fetchAll();
+  return resources;
+
 }
