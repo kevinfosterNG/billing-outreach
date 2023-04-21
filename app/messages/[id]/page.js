4dc650297faf7257c563d9e4adb8403ba256c401
Kevin Foster <kevinfoster@nextcare.com>
2023-04-21 16:05:14 -0400

    how to spell domain

--- app/messages/[id]/page.js
+++ app/messages/[id]/page.js
@@ -5,7 +5,7 @@ const messageUrl = process.env.NEXT_PUBLIC_APP_URL+":"+process.env.PORT + "/api/
 async function getMessage(id) {
     //const messageUrl = `${process.env.NEXT_PUBLIC_APP_URL}:${process.env.PORT}/api/messages/${id}`;
     let messageUrl = process.env.NEXT_PUBLIC_APP_URL+":"+process.env.PORT + "/api/messages/" + id;
-    if (messageUrl.includes("railay.app"))
+    if (messageUrl.includes("railway.app"))
         messageUrl = messageUrl.replace(":80","");
 
     console.log("Fetching from: ",messageUrl);
